76be139bf6c60ea620a5afaec2f3b608e2e10774
Brenton Poke <brentonpoke@outlook.com>
2020-10-09 01:38:59 -0400

    changing exception handler

--- src/main/java/graph/backend/Controller/GlobalExceptionHandlerController.java
+++ src/main/java/graph/backend/Controller/GlobalExceptionHandlerController.java
@@ -3,37 +3,33 @@ package graph.backend.Controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import com.rollbar.notifier.Rollbar;
-import java.time.LocalTime;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import static com.rollbar.notifier.config.ConfigBuilder.withAccessToken;
 
 @ControllerAdvice
-@EnableWebMvc
-public class GlobalExceptionHandlerController {
+public class GlobalExceptionHandlerController extends ResponseEntityExceptionHandler {
 
-    public static final String DEFAULT_ERROR_VIEW = "error";
     final Rollbar rollbar;
     
     @Autowired
     public GlobalExceptionHandlerController(Rollbar rollbar) {
         this.rollbar = rollbar;
     }
-    
     @ExceptionHandler(value = Exception.class)
-    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("exception", e);
-        mav.addObject("url", req.getRequestURL());
-        mav.addObject("timestamp",LocalTime.now());
-        mav.setViewName(DEFAULT_ERROR_VIEW);
-        rollbar.error(e,mav.getModelMap());
-        return mav;
+    protected ResponseEntity<Object> handleConflict(
+        RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = ex.getCause().getMessage();
+        return handleExceptionInternal(ex, bodyOfResponse,
+            new HttpHeaders(), HttpStatus.CONFLICT, request);
     }
 }
