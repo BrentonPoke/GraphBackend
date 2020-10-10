097e31f88065eddcc90c3a7b8a78b7e923b9cf22
Brenton Poke <brentonpoke@outlook.com>
2020-10-10 18:02:56 -0400

    harden the build script

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 mvn -f pom.xml clean -DskipTests package
 docker build -t neo4jbackend -f Dockerfile
+if [ -x "$(command -v podman-compose)" ]; then
+  podman-compose up -d >&2
+  exit 1
+fi
 docker-compose up -d
