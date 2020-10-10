c111135c90da8384db556c7ed55a13f9359caacc
Brenton Poke <brentonpoke@outlook.com>
2020-10-10 01:12:43 -0400

    figured out what it was...

--- src/main/java/graph/backend/Beans/QueryObjects/AssignAnimalQuery.java
+++ src/main/java/graph/backend/Beans/QueryObjects/AssignAnimalQuery.java
@@ -2,11 +2,15 @@ package graph.backend.Beans.QueryObjects;
 
 import graph.backend.Beans.Animal;
 import java.util.Set;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Singular;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class AssignAnimalQuery {
 String username;
--- src/main/java/graph/backend/Beans/QueryObjects/AssignFoodQuery.java
+++ src/main/java/graph/backend/Beans/QueryObjects/AssignFoodQuery.java
@@ -2,12 +2,16 @@ package graph.backend.Beans.QueryObjects;
 
 import graph.backend.Beans.Animal;
 import java.util.Set;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Singular;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class AssignFoodQuery {
   String foodName;
