dd88c22c6420c09bbfb6b100be3e4e3a28672f9a
Brenton Poke <brentonpoke@outlook.com>
2020-11-05 23:35:08 -0500

    change tag to latest in shell script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 mvn -f pom.xml clean -DskipTests package
 if [ -x "$(command -v podman)" ]; then
   podman build -t neo4jbackend -f Dockerfile
-  else docker build -t neo4jbackend -f Dockerfile
+  else docker build -t latest -f Dockerfile
 fi
 
 if [ -x "$(command -v podman-compose)" ]; then
